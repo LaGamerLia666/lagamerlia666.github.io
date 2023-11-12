8d94792ea7a56d5f1ccd312211496d94babb6ea5
d4hlia <lagamerlia666@gmail.com>
2023-11-12 20:09:45 -0300

    Create header.js
    made this so i dont have to edit every single file in the repo ifi decide to change the header

---
+++ scripts/header.js
@@ -0,0 +1,4 @@
+function addCode()
+{
+	document.getElementById("menubar").innerHTML += '<img src="img/d4hlia-logo.png" alt="d4hlia" class="logo"><ul class="navbar"><li class="nav-item"><a href="home.html">Home</a></li><li class="nav-item"><a href="projects.html">Projects</a><!-- i wont make this shit now <ul class="submenu"><li><a href="projects/arduino-music.html">Arduino Music</a></li><li><a href="projects/bombseeker-3d.html">BombSeeker 3D</a></li><li><a href="projects/wyslmiiwaw.html">Worm Dating Sim</a></li></ul>--></li><li class="nav-item"><a href="gallery.html">Gallery</a></li><li class="nav-item"><a href="about.html">About me</a></li><li class="nav-item"><a href="contact.html">Contact</a></li></ul>';
+}
