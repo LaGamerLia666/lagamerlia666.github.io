8424f5337a220eac1a26864f1860a1d08d09fdb7
d4hlia <lagamerlia666@gmail.com>
2023-12-25 21:09:41 -0300

    nuked

--- scripts/header.js
+++
@@ -1,4 +0,0 @@
-function addHead()
-{
-	document.getElementById("menubar").innerHTML += '<img src="img/d4hlia-logo.png" alt="d4hlia" class="logo"><ul class="navbar"><li class="nav-item"><a href="home.html">Home</a></li><li class="nav-item"><a href="projects.html">Projects</a></li><li class="nav-item"><a href="gallery.html">Gallery</a></li><li class="nav-item"><a href="about.html">About me</a></li><li class="nav-item"><a href="contact.html">Contact</a></li></ul>';
-}
