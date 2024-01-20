5e2a53981ebff4fc3f8a241c19e0d4c49eacc938
d4hlia <lagamerlia666@gmail.com>
2024-01-19 21:24:29 -0300

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,4 @@
 function addHead()
 {
-	document.getElementById("menubar").innerHTML += '<img src="img/d4hlia-logo.png" alt="d4hlia" class="logo"><ul class="navbar"><li class="nav-item"><a href="home.html">Home</a></li><li class="nav-item"><a href="https://en.pronouns.page/@lagamerlia666" target="_blank">Pronouns</a></li><li class="nav-item"><a href="interests.html">Interests</a></li><li class="nav-item"><a href="dni.html">DNI</a></li><li class="nav-item"><a href="contact.html">Contact</a></li></ul>';
+	document.getElementById("menubar").innerHTML += '<img src="img/d4hlia-logo.png" alt="d4hlia" class="logo"><ul class="navbar"><li class="nav-item"><a href="home.html">Home</a></li><li class="nav-item"><a href="https://pronouns.cc/@lagamerlia666" target="_blank">Pronouns</a></li><li class="nav-item"><a href="interests.html">Interests</a></li><li class="nav-item"><a href="dni.html">DNI</a></li><li class="nav-item"><a href="contact.html">Contact</a></li></ul>';
 }
